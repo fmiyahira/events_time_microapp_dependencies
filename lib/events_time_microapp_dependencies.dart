b5180e6fa33882a6851b0f1ff7f7cdaf2aad05e8
Fernando Augusto Miyahira <miyahirafernando@gmail.com>
2023-08-02 20:38:20 -0400

    adding brasil_fields and email validation

--- lib/events_time_microapp_dependencies.dart
+++ lib/events_time_microapp_dependencies.dart
@@ -1,3 +1,6 @@
+export 'package:brasil_fields/brasil_fields.dart';
+export 'package:email_validator/email_validator.dart';
+
 export './src/injector/all.dart';
 export './src/local_database/all.dart';
 export './src/local_storage/all.dart';
